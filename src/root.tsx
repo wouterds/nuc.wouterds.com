04ed584bade2773a09bc3579665f6223ff791a5d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2024-10-13 16:12:36 +0200

    feat: init remix

---
+++ src/root.tsx
@@ -0,0 +1,42 @@
+import './tailwind.css';
+
+import type { LinksFunction } from '@remix-run/node';
+import { Links, Meta, Outlet, Scripts } from '@remix-run/react';
+import { ReactNode } from 'react';
+
+export const links: LinksFunction = () => [
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+  {
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
+    crossOrigin: 'anonymous',
+  },
+  {
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+  },
+];
+
+export const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        {/* <ScrollRestoration /> */}
+        <Scripts />
+      </body>
+    </html>
+  );
+};
+
+const App = () => {
+  return <Outlet />;
+};
+
+export default App;
---
+++ src/routes/_index.tsx
@@ -0,0 +1,36 @@
+import type { MetaFunction } from '@remix-run/node';
+import { useLoaderData, useRevalidator } from '@remix-run/react';
+import { useEffect } from 'react';
+
+export const meta: MetaFunction = () => {
+  return [{ title: 'nuc.wouterds.be' }];
+};
+
+export const loader = async () => {
+  const data = await fetch(process.env.API_URL!);
+
+  return data;
+};
+
+export default function Index() {
+  const data = useLoaderData<typeof loader>();
+  const { revalidate, state } = useRevalidator();
+
+  useEffect(() => {
+    if (state === 'idle') {
+      const timeout = setTimeout(() => {
+        revalidate();
+      }, 1000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [state, revalidate]);
+
+  return (
+    <div className="font-mono text-xs p-6">
+      <pre className="whitespace-pre-wrap">
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </div>
+  );
+}
